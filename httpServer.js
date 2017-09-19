ea62b4a12f9c3bed7b63c78400b81636621d6f4f
SJB <StevenJBurns@users.noreply.github.com>
2017-09-18 20:29:22 -0600

    Working Solutions PLUS Bonuses for Part 1 and 2; Express Parts 3 and 4 next

--- httpServer.js
+++ httpServer.js
@@ -11,170 +11,105 @@ let pathToPetShop = path.join(__dirname, "pets.json");
 const regexPetsPathname = /^\/pets\/(.*)$/;
 
 const server = http.createServer((req, res) => {
+  // get the url path and then its pathname
+  let urlParsed = url.parse(req.url);
+  let pathnameParsed = urlParsed.pathname;
+
+  // Remove "/" characters then remove the leading empty string after the split()... e.g. ["", "pets", "0"] -> ["pets, "0"]
+  pathnameParsed = urlParsed.pathname.split(path.sep);
+  pathnameParsed.shift();
+
+  // If you arent looking for the pets path, you're in the wrong app
+  if (pathnameParsed[0] != "pets") return sendError404(res);
 
   switch (req.method) {
     case "GET" :
-      handleGET(url.parse(req.url), res);
+      handleGET(res, pathnameParsed[1]);
       break;
 
     case "POST" :
-      handlePOST(url.parse(req.url), res);
+      handlePOST(req, res);
       break;
 
     default :
-      http.METHODS.includes(req.method) ? res.statusCode = 501 : res.statusCode = 400
-      res.setHeader("Content-Type", "text/plain");
-      http.METHODS.includes(req.method) ? res.end("Valid Method But Implemented On This Server") : res.end("Check your HTTP Method")
+      sendError400(res);
     }
   });
 
+function sendError400(res) {
+  res.writeHead(400, {"Content-Type" : "text/plain"});
+  res.end("Bad Request");
+}
 
-//   if (req.method === "GET" && req.url === "/pets") {
-//     fs.readFile(petsPath, "utf8", function(err, petsJSON) {
-//       if (err) {
-//         console.error(err);
-//         res.statusCode = 500;
-//         res.setHeader("Content-Type", "text/plain");
-//         return res.end("Internal Server Error");
-//       }
-//       res.setHeader("Content-Type", "application/json");
-//       res.end(petsJSON);
-//     })
-//   } else if (req.method === "GET" && req.url === "/pets/0") {
-//     fs.readFile(petsPath, "utf8", function(err, petsJSON) {
-//       if (err) {
-//         console.error(err);
-//         res.statusCode = 500;
-//         res.setHeader("Content-Type", "text/plain");
-//         return res.end("Internal Server Error");
-//       }
-//
-//       let pets = JSON.parse(petsJSON);
-//       let petJSON = JSON.stringify(pets[0]);
-//
-//       res.setHeader("Content-Type", "application/json");
-//       res.end(petJSON);
-//     })
-//   } else if (req.method === "GET" && req.url === "/pets/1") {
-//     fs.readFile(petsPath, "utf8", function(err, petsJSON) {
-//       if (err) {
-//         console.error(err);
-//         res.statusCode = 500;
-//         res.setHeader("Content-Type", "text/plain");
-//         return res.end("Internal Server Error");
-//       }
-//
-//       let pets = JSON.parse(petsJSON);
-//       let petJSON = JSON.stringify(pets[1]);
-//
-//       res.setHeader("Content-Type", "application/json");
-//       res.end(petJSON);
-//     })
-//   } else if (req.method === "POST" && req.url === "/pets") {
-//
-//     let returnChunk = "";
-//
-//     req.on("data", (chunk) => returnChunk += chunk.toString());
-//
-//     req.on("end", function() {
-//       fs.readFile(petsPath, "utf8", function(err, petsJSON) {
-//         if (err) {
-//           console.error(err.stack);
-//           res.statusCode = 500;
-//           res.setHeader("Content-Type", "text/plain");
-//           return res.end("Internal Server Error");
-//         }
-//
-//         let pets = JSON.parse(petsJSON);
-//         let body = JSON.parse(returnChunk);
-//
-//         let age = Number.parseInt(body.age);
-//         let kind = body.kind;
-//         let name = body.name;
-//
-//         if (Number.isNaN(age) || !kind || !name) {
-//           res.statusCode = 400;
-//           res.setHeader("Content-Type", "text/plain");
-//           res.end("Bad Request");
-//           return;
-//         }
-//
-//         let pet = {age, kind, name};
-//         pets.push(pet);
-//
-//         let updatedPetsJSON = JSON.stringify(pets);
-//         let petJSON = JSON.stringify(pet);
-//
-//         fs.writeFile(petsPath, updatedPetsJSON, function(err) {
-//           if (err) {
-//             console.error(err.stack);
-//
-//             res.statusCode = 500;
-//             res.setHeader("Content-Type", "text/plain");
-//             res.end("Internal Server Error");
-//             return;
-//           }
-//
-//           res.setHeader("Content-Type", "application/json");
-//           res.end(petJSON);
-//         })
-//       })
-//     })
-//   } else {
-//     res.statusCode = 404;
-//     res.setHeader("Content-Type", "text/plain");
-//     res.end("Not Found");
-//   }
-// });
+function sendError404(res) {
+  res.writeHead(404, {"Content-Type" : "text/plain"});
+  res.end("Not Found");
+}
 
+function sendError500(res, error) {
+  console.error(console.error(error));
+  res.writeHead(500, {"Content-Type" : "text/plain"});
+  res.end("Internal Server Error");
+}
 
-function handleGET(reqUrl, res) {
+function handleGET(res, requestedIndex) {
   fs.readFile(pathToPetShop, "utf8", (readError, data) => {
-    if (readError) console.error(readError);
+    if (readError) sendError500(res, readError);
 
-    // Test url.pathname against the regex and set statusCode as required
-    regexPetsPathname.test(reqUrl.pathname) ? res.statusCode = 200 : () => res.statusCode = 404
+    let storedPets = JSON.parse(data);  // get ALL the pets...
 
-    // get ALL the pets...
-    let storedPets = JSON.parse(data);
-    // break the pathname up into strings without the "/" symbols
-    let pathnameSplit = reqUrl.pathname.split(path.sep);
-    // remove the leading empty string after the split()... e.g. ["", "pets", "0"] -> ["pets, "0"]
-    pathnameSplit.shift();
-
-    if (!pathnameSplit[1]) {
-      res.setHeader("Content-Type", "application/json");
-      res.end(data);
-    } else if (!storedPets[pathnameSplit[1]]) {
-      res.statusCode = 404;
-      res.setHeader("Content-Type", "text/plain");
-      res.end("Not Found");
+    if (requestedIndex < 0 || requestedIndex > (storedPets.length - 1)) {
+      sendError404(res);
+    } else if (requestedIndex === undefined) {
+      res.writeHead(200, {"Content-Type" : "application/json"});
+      res.end(JSON.stringify(storedPets));
     } else {
-      res.setHeader("Content-Type", "application/json");
-      res.end(JSON.stringify(storedPets[pathnameSplit[1]]));
+      res.writeHead(200, {"Content-Type" : "application/json"});
+      res.end(JSON.stringify(storedPets[requestedIndex]));
     }
   });
 }
 
-function handlePOST(reqURL, res) {
-  fs.readFile(pathToPetShop, "utf8", (readError, petsJSON) => {
-    if (readError) console.error(readError);
+function handlePOST(req, res) {
+  fs.readFile(pathToPetShop, "utf8", (readError, data) => {
+    if (readError) sendError500(res, readError);
+
+    let completeChunk = "";
+
+    req.on("data", (chunk) => completeChunk += chunk.toString());
+    req.on("end", () => {
+      fs.readFile(pathToPetShop, "utf8", (readError, data) => {
+        if (readError) sendError500(res, readError);
+
+        let storedPets = JSON.parse(data);  // get ALL the pets...
+        let newPet = JSON.parse(completeChunk);
+
+        if (!newPet["age"] || !newPet["kind"] || !newPet["name"]) sendError400(res);
+
+        storedPets.push(newPet);
+
+        fs.writeFile(pathToPetShop, JSON.stringify(storedPets), (writeError) => {
+          if (writeError) sendError500(res, writeError);
+
+          res.writeHead(200, {"Content-Type" : "application/json"});
+          res.end(JSON.stringify(newPet));
+        });
+      });
+    });
+  });
+}
+
+function handlePut (response, reqURL) {
+  fs.readFile(pathToPetShop, "utf8", (readError, data) => {
+    if (readError) sendError500(res, readError);
 
     let storedPets = JSON.parse(data);
   });
 }
 
-function handlePut (reqURL, res) {
-  fs.readFile(pathToPetShop, "utf8", (readError, petsJSON) => {
-    if (readError) console.error(readError);
-
-    let storedPets = JSON.parse(data);
-  });
-}
-
-function handleDELETE (reqURL, res) {
-  fs.readFile(pathToPetShop, "utf8", (readError, petsJSON) => {
-    if (readError) console.error(readError);
+function handleDELETE (response, reqURL) {
+  fs.readFile(pathToPetShop, "utf8", (readError, data) => {
+    if (readError) sendError500(res, readError);
 
     let storedPets = JSON.parse(data);
   });
@@ -182,4 +117,4 @@ function handleDELETE (reqURL, res) {
 
 server.listen(port, () => console.log(`HTTP Server is running. Listening On Port ${port}`));
 
-module.exports = server
+module.exports = server;
