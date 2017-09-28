6c9ef2110cfd3673e69de077228b21b9e8ebb248
SJB <StevenJBurns@users.noreply.github.com>
2017-09-28 10:21:54 -0600

    All Parts and Bonuses complete

--- restfulExpressServer.js
+++ restfulExpressServer.js
@@ -2,6 +2,7 @@
 
 let express = require("express");
 let bodyParser = require("body-parser");
+let basicAuth = require("basic-auth");
 let morgan = require("morgan");
 let path = require("path");
 let fs = require("fs");
@@ -10,11 +11,22 @@ let app = express();
 let port = process.env.PORT || 8000;
 let pathToPetShop = path.join(__dirname, "pets.json");
 
-app.disable("x-powered-by");
-
 app.use(morgan("short"));
 
 app.use(bodyParser.json());
+
+app.disable("x-powered-by");
+
+app.use((req, res, next) => {
+  let credentials = basicAuth(req);
+
+  if (!credentials || credentials.name != "admin" || credentials.pass != "meowmix") {
+    res.set('WWW-Authenticate', 'Basic realm="Required"');
+    res.sendStatus(401);
+  } else {
+    return next();
+  }
+});
 
 app.get("/pets", (req, res, next) => {
   fs.readFile(pathToPetShop, "utf8", (readError, data) =>
@@ -55,6 +67,7 @@ app.patch("/pets/:id", (req, res, next) => {
 
     if (requestedID < 0 && requestedID >= allPets.length - 1) next();
 
+    // Straight out of Crushing Candy Code !!!
     for (let key of ["age", "kind", "name"]) {
       if (req.body.hasOwnProperty(key)) allPets[requestedID][key] = req.body[key];
     }
